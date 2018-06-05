983ec686110edaf197fe822fb56c09c62a266421
Raitis Ralfs Vecmanis <raitis-ralfs.vecmanis@venta.lv>
2018-06-05 10:53:42 +0300

    Init commit

---
+++ src/main/java/me/kursaDarbs/app/KursaDarbsJava2018Application.java
@@ -0,0 +1,12 @@
+package me.kursaDarbs.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursaDarbsJava2018Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KursaDarbsJava2018Application.class, args);
+	}
+}
