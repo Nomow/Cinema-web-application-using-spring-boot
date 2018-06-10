3c0c89ae72275ef07939032b2a29bc8544502ea6
Alvils Sture <alvils.sture@venta.lv>
2018-06-10 18:46:09 +0300

    added views

--- src/main/java/me/kursaDarbs/app/controller/IndexController.java
+++ src/main/java/me/kursaDarbs/app/controller/IndexController.java
@@ -1,4 +1,16 @@
 package me.kursaDarbs.app.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Controller
 public class IndexController {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
 }
+
