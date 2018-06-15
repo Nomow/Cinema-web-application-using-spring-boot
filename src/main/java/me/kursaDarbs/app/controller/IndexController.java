283472b5d047b7690790e64d69e25662bcd37c22
Alvils Sture <alvils.sture@venta.lv>
2018-06-15 18:18:56 +0300

    linked css js folders

--- src/main/java/me/kursaDarbs/app/controller/IndexController.java
+++ src/main/java/me/kursaDarbs/app/controller/IndexController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
     @RequestMapping("/")
     public String index() {
-        return "index";
+        return "home";
     }
 
 }
