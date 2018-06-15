32065cc16064c046ffebee25b504e3c747b22cf4
Alvils Sture <alvils.sture@venta.lv>
2018-06-15 18:55:26 +0300

    added iterative loop for cinema cards

--- src/main/java/me/kursaDarbs/app/controller/IndexController.java
+++ src/main/java/me/kursaDarbs/app/controller/IndexController.java
@@ -10,13 +10,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class IndexController {
+    @Autowired CinemaRepository cinemaRepository;
     @RequestMapping("/")
-    public String index() {
-        return "home";
+    public ModelAndView Index() {
+        ModelAndView mav = new ModelAndView();
+        List<Cinema> cinemas = cinemaRepository.findAll();
+        mav.setViewName("home");
+        mav.getModelMap().addAttribute("cinemas", cinemas);
+        return mav;
     }
-
 }
 
