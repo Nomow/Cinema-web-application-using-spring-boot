4e97583f9b6fcfa9b5450184dabaff6f15f55d90
Raitis Ralfs Vecmanis <raitis-ralfs.vecmanis@venta.lv>
2018-06-21 16:22:07 +0300

    Add routing links

--- src/main/java/me/kursaDarbs/app/controller/AdminController.java
+++ src/main/java/me/kursaDarbs/app/controller/AdminController.java
@@ -9,9 +9,33 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AdminController {
+    // ======== Get all =============
     @RequestMapping(value = "/admin", method = RequestMethod.GET)
-    public String GetCinemaList() {
-        return "index";
+    public String GetAdminPanel() {
+        return "admin/index";
     }
 
+    @RequestMapping(value = "/admin/halls/cinema/{cinemaID}", method = RequestMethod.GET)
+    public String GetAdminCinemaHalls() {
+        return "admin/halls";
+    }
+    // ===============================
+
+    // ===== Get individual ==========
+    // ===== (For editing form) ======
+    @RequestMapping(value = "/admin/cinema/{cinemaID}", method = RequestMethod.GET)
+    public String GetAdminCinema() {
+        return "admin/cinema";
+    }
+
+    @RequestMapping(value = "/admin/hall/{hallID}", method = RequestMethod.GET)
+    public String GetAdminHall() {
+        return "admin/hall";
+    }
+
+    @RequestMapping(value = "/admin/session/{sessionID}", method = RequestMethod.GET)
+    public String GetAdminSession() {
+        return "admin/session";
+    }
+    // ===============================
 }
